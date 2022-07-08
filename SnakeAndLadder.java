06545005a1f15ff310dff5d3417e5cf731a68fc9
sahilkhan1996 <sahilkhan199655@gmail.com>
2022-07-08 19:03:22 +0530

    Player reached to final position

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -9,6 +9,9 @@ public class SnakeAndLadder {
 		System.out.println("The position of player is: " + playerposition);
 
 		// uc2: the player roll the die for the 1st time.
+		
+		 while (playerposition < 100)
+         {
 		Random r = new Random();
 		int dienumber = r.nextInt(1, 7);
 		System.out.println("player Rolling die rolling and number comes is: " + dienumber);
@@ -31,5 +34,13 @@ public class SnakeAndLadder {
                 System.out.println("No play");
                 break;
         }
+        if (playerposition < 0)
+        {
+            playerposition = 0;
+        }
+        System.out.println("final position checker: " + playerposition);
+    }
+      System.out.println("player beat the snakes and completed the game" + playerposition);
+
 	}
 }
